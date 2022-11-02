cdc941335c3f034bce6f527681e87f30067c3996
林鼎盛 <lindsh@yonyou.com>
2022-11-02 21:50:40 +0800

    feat:收起展开

--- rickie/js/main.js
+++ rickie/js/main.js
@@ -22,10 +22,31 @@ function scrollById(menuId, goalId){
     document.getElementById(menuId).addEventListener('click',
         (e) => scroll(document.getElementById(goalId)))
 }
+function expand(expandEle,goalEle,height){
+    if (goalEle.style.maxHeight==='unset'){
+        goalEle.style.maxHeight=`${height}px`
+        expandEle.innerHTML='[Expand]'
+    }else {
+        goalEle.style.maxHeight='unset'
+        expandEle.innerHTML='[Collapse]'
+    }
+}
+// 传两个id进来，实现展开收起
+function expandById(expandId, goalId, height){
+    let expandEle=document.getElementById(expandId)
+    let goalEle=document.getElementById(goalId)
+    expandEle.addEventListener('click',
+        (e) => expand(expandEle,goalEle,height))
+}
+
 (function (){
+    // 导航栏点击跳转事件
     scrollById('toHome', 'toptitle')
     scrollById('toPublications', 'Publications')
     scrollById('toPatents','Patents')
     scrollById('toExperiences','Experiences')
     scrollById('toMiscellaneous','Miscellaneous')
+    // 点击收起展开事件
+    expandById('ExpandPublications', 'Publications',260)
+
 })()
